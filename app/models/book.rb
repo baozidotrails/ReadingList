1b52fbdd4e1d071bbe2429acb4a509565c1e373d
Baozi <baozi.rails@gmail.com>
2014-09-19 18:33:07 +0800

    Add genres filter

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,18 @@
 class Book < ActiveRecord::Base
+
+  has_many :book_genres
+  has_many :genres, through: :book_genres
+
   scope :finished, -> { where("finished_on IS NOT NULL") }
   scope :recent  , -> { where("finished_on > ?", 2.days.ago) }
 
   # scope always reture the collection
-  scope :search  , -> (keyword) {
+  scope :search, -> (keyword) {
     where("keywords LIKE ?", "%#{keyword.downcase}%") if keyword.present?
+  }
+
+  scope :filter, -> (genre_name) {
+    joins(:genres).where(genres: { name: genre_name }) if genre_name.present?
   }
 
   before_save :set_keywords
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 Book.destroy_all
+Genre.destroy_all
+
+tutorial = Genre.create!(name: 'Tutorial')
+advanced = Genre.create!(name: 'Advanced')
+manual   = Genre.create!(name: 'Manual')
+
 Book.create!([
   {
     title:       "The Rails 4 Way",
@@ -6,7 +12,8 @@ Book.create!([
     description: "The Rails™ 4 Way is the only comprehensive, authoritative guide to delivering production-quality code with Rails 4. Kevin Faustino joins pioneering Rails developer Obie Fernandez to illuminate the entire Rails 4 API, including its most powerful and modern idioms, design approaches, and libraries. They present extensive new and updated content on security, performance, caching, Haml, RSpec, Ajax, the Asset Pipeline, and more.",
     amazon_id:   "0321944275",
     rating:      5,
-    finished_on: 10.days.ago
+    finished_on: 10.days.ago,
+    genres: [manual, advanced]
   },
   {
     title:       "Rails AntiPatterns",
@@ -14,7 +21,8 @@ Book.create!([
     description: "The Complete Guide to Avoiding and Fixing Common Rails 3 Code and Design Problems As developers worldwide have adopted the powerful Ruby on Rails web framework, many have fallen victim to common mistakes that reduce code quality, performance, reliability, stability, scalability, and maintainability. Rails™ AntiPatterns identifies these widespread Rails code and design problems, explains why they’re bad and why they happen—and shows exactly what to do instead. ",
     amazon_id:   "0321604814",
     rating:      4,
-    finished_on: 5.days.ago
+    finished_on: 5.days.ago,
+    genres: [manual, advanced]
   },
   {
     title:       "Practical Object-Oriented Design in Ruby",
@@ -22,7 +30,17 @@ Book.create!([
     description: "The Complete Guide to Writing More Maintainable, Manageable, Pleasing, and Powerful Ruby Applications Ruby’s widely admired ease of use has a downside: Too many Ruby and Rails applications have been created without concern for their long-term maintenance or evolution. The Web is awash in Ruby code that is now virtually impossible to change or extend. This text helps you solve that problem by using powerful real-world object-oriented design techniques, which it thoroughly explains using simple and practical Ruby examples.",
     amazon_id:   "0321721330",
     rating:      5,
-    finished_on: nil
+    finished_on: nil,
+    genres: [manual]
+  },
+  {
+    title:       "Ruby on Rails Tutorial",
+    author:      "Michael Hartl",
+    description: "“Ruby on Rails™ Tutorial by Michael Hartl has become a must-read for developers learning how to build Rails apps.” —Peter Cooper, Editor of Ruby Inside Using Rails, developers can build web applications of exceptional elegance and power. Although its remarkable capabilities have made Ruby on Rails one of the world’s most popular web development frameworks, it can be challenging to learn and use. Ruby on Rails™ Tutorial, Second Edition, is the solution. Best-selling author and leading Rails developer Michael Hartl teaches Rails by guiding you through the development of your own complete sample application using the latest techniques in Rails web development. The updates to this edition include all-new site design using Twitter’s Bootstrap; coverage of the new asset pipeline, including Sprockets and Sass; behavior-driven development (BDD) with Capybara and RSpec; better automated testing with Guard and Spork; roll your own authentication with has_secure_password; and an introduction to Gherkin and Cucumber.",
+    amazon_id:   "0321832051",
+    rating:      5,
+    finished_on: nil,
+    genres: [tutorial]
   }
 ])
 
